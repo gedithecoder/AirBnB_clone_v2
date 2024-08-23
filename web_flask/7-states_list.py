163cfd4efcdcc479b428107cedf62a533f99b2e9
gedithecoder <gedionderejev.com>
2024-08-23 14:43:32 -0700

    AirBnB_clone_v2

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,32 +1,50 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states_list: HTML page with a list of all State objects in DBStorage.
+<<<<<<< HEAD
+"""the `7-states_list` module
+=======
 """
-from models import storage
-from flask import Flask
-from flask import render_template
+>>>>>>> 212529db4b955a72d996660137790c2497399ff5
+starts a Flask web application
+"""
 
+from flask import Flask, render_template
+<<<<<<< HEAD
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/states_list")
+def states_list():
+    """fetchs the list of states from the database and displays it"""
+    states = sorted(list(storage.all(State).values()), key=lambda x: x.name)
+    return render_template("7-states_list.html", states=states)
+=======
+from models import *
+from models import storage
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-
-    States are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
+>>>>>>> 212529db4b955a72d996660137790c2497399ff5
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
+<<<<<<< HEAD
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
+=======
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
+>>>>>>> 212529db4b955a72d996660137790c2497399ff5
